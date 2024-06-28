3925ea30076bea79e4650ca6e954dae99e153a82
preetikumari5 <147963preetikumari@gmail.com>
2024-06-28 10:44:30 +0530

    added project2

---
+++ JAVA SCRIPT/Project 2/app.js
@@ -0,0 +1,17 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const playGame = () => {
+
+}
+
+choices.forEach((choice) => {
+    
+    choice.addEventListener("click",() => {
+        const userChoice = choice.getAttribute("id")
+        console.log("choice was clicked" , userChoice);
+        playGame(userChoice);
+    })
+})
